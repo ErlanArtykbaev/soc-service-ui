db3ece740c3f633d398a544cf3241b15ee729348
ulookbek <ulukbekkaparbekov@gmail.com>
2020-10-22 15:23:14 +0600

    Remove header from header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -47,7 +47,7 @@ const Header = () => {
                     </ul>
                 </div>
             </nav>
-            <MainHeader/>
+
         </>
     )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import MainHeader from "../components/Home/MainHeader";
 
 const Home = () => {
-  return(
-    <div className='home'>
-
-    </div>
-  )
+    return (
+        <div className='home'>
+            <MainHeader/>
+        </div>
+    )
 }
 
 export default Home
