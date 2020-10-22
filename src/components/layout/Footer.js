6e02021ed247cc07e0055ba35ccb7f80d266b8f0
erlan artykbaev <erik.2405@mail.ru>
2020-10-22 16:42:12 +0600

    package update

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,15 +3,13 @@ import {NavLink} from "react-router-dom";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 
 const Footer = () => {
-    return (
-        <footer className="page-footer font-small blue bg-primary">
-
-            <div className="footer-copyright text-center py-3">© 2018 Copyright:
-                <NavLink to={'/'}> MDBootstrap.com</NavLink>
-            </div>
-
-        </footer>
-);
+	return (
+		<footer className="page-footer font-small blue bg-primary">
+			<div className="footer-copyright text-center py-3">© 2018 Copyright:
+				<NavLink to={'/'}> MDBootstrap.com</NavLink>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
