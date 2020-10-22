39803852bafe1a2e75d00b8a59c3286ff3649844
ulookbek <ulukbekkaparbekov@gmail.com>
2020-10-22 14:15:27 +0600

    Add material ui

--- src/App.js
+++ src/App.js
@@ -8,23 +8,23 @@ import Header from './components/layout/Header'
 import About from './pages/About'
 
 function App() {
-  return (
-		<div className='app'>
-			<Router>
-				<Header />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/admin' component={Admin} />
-          <Route path='/about' component={About} />
-          <Route path='/team' component={About} />
-          <Route path='/projects' component={About} />
-          <Route path='/blog' component={About} />
-          <Route path='/career' component={About} />
-          <Route path='/contactus' component={About} />
-        </Switch>
-			</Router>
-		</div>
-  )
+    return (
+        <div className='app'>
+            <Router>
+                <Header/>
+                <Switch>
+                    <Route path='/' exact component={Home}/>
+                    <Route path='/admin' component={Admin}/>
+                    <Route path='/about' component={About}/>
+                    <Route path='/team' component={About}/>
+                    <Route path='/projects' component={About}/>
+                    <Route path='/blog' component={About}/>
+                    <Route path='/career' component={About}/>
+                    <Route path='/contactus' component={About}/>
+                </Switch>
+            </Router>
+        </div>
+    )
 }
 
 export default App
