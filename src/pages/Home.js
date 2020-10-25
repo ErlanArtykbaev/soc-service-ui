8f73f072a73ecc85dc551745a3a2aafe2a2a8c1b
ulookbek <ulukbekkaparbekov@gmail.com>
2020-10-25 19:36:11 +0600

    add ourPartners, emailUs, footer base blocks

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,143 +3,146 @@ import MainHeader from '../components/Home/MainHeader'
 import TechnologiesCard from '../components/TechnologiesCard/TechnologiesCard'
 import WeUseCard from '../components/TechnologiesCard/WeUseCard/WeUseCard'
 import Slider from '../containers/Slider'
+import ourPartners from  './../assets/img/ourPartners.jpg';
+import FeedBack from "../components/Home/FeedBack/FeedBack";
 
 const Home = () => {
-  return (
-    <div className="home">
-      <MainHeader />
-      <Slider />
+    return (
+        <div className="home">
+            <MainHeader/>
+            <Slider/>
 
-      <section className="container">
-        <div className="row row--large-gutter@xl-up u-pad-ends-small@lg-up u-pad-ends-2xsmall@ms-down">
-          <TechnologiesCard
-            title={'Expertise in'}
-            subtitle={'backend development'}
-            description={
-              'We can provide\n' +
-              'language-agnostic development,\n' +
-              'SOLID development, test-driven development, RESTFul API, scalable\n' +
-              'architecture, third-party\n' +
-              'integrations. Our passion is creating solid yet scalable solutions such\n' +
-              'as Cloud/Backend Architecture,\n' +
-              'API design and DevOps.'
-            }
-            WeUse={
-              <WeUseCard
-                stacks={[
-                  'PHP',
-                  'JS',
-                  'NodeJs',
-                  'C#',
-                  'Rust',
-                  'Python',
-                  'Django',
-                  'Java',
-                  '.NET',
-                  'React',
-                  'VueJS',
-                  'AngularJS',
-                ]}
-              />
-            }
-          />
-          <TechnologiesCard
-            title={'Expertise in'}
-            subtitle={'backend development'}
-            description={
-              'We can provide\n' +
-              'language-agnostic development,\n' +
-              'SOLID development, test-driven development, RESTFul API, scalable\n' +
-              'architecture, third-party\n' +
-              'integrations. Our passion is creating solid yet scalable solutions such\n' +
-              'as Cloud/Backend Architecture,\n' +
-              'API design and DevOps.'
-            }
-            WeUse={
-              <WeUseCard
-                stacks={[
-                  'PHP',
-                  'JS',
-                  'NodeJs',
-                  'C#',
-                  'Rust',
-                  'Python',
-                  'Django',
-                  'Java',
-                  '.NET',
-                  'React',
-                  'VueJS',
-                  'AngularJS',
-                ]}
-              />
-            }
-          />
-          <TechnologiesCard
-            title={'Expertise in'}
-            subtitle={'backend development'}
-            description={
-              'We can provide\n' +
-              'language-agnostic development,\n' +
-              'SOLID development, test-driven development, RESTFul API, scalable\n' +
-              'architecture, third-party\n' +
-              'integrations. Our passion is creating solid yet scalable solutions such\n' +
-              'as Cloud/Backend Architecture,\n' +
-              'API design and DevOps.'
-            }
-            WeUse={
-              <WeUseCard
-                stacks={[
-                  'PHP',
-                  'JS',
-                  'NodeJs',
-                  'C#',
-                  'Rust',
-                  'Python',
-                  'Django',
-                  'Java',
-                  '.NET',
-                  'React',
-                  'VueJS',
-                  'AngularJS',
-                ]}
-              />
-            }
-          />
-          <TechnologiesCard
-            title={'Expertise in'}
-            subtitle={'backend development'}
-            description={
-              'We can provide\n' +
-              'language-agnostic development,\n' +
-              'SOLID development, test-driven development, RESTFul API, scalable\n' +
-              'architecture, third-party\n' +
-              'integrations. Our passion is creating solid yet scalable solutions such\n' +
-              'as Cloud/Backend Architecture,\n' +
-              'API design and DevOps.'
-            }
-            WeUse={
-              <WeUseCard
-                stacks={[
-                  'PHP',
-                  'JS',
-                  'NodeJs',
-                  'C#',
-                  'Rust',
-                  'Python',
-                  'Django',
-                  'Java',
-                  '.NET',
-                  'React',
-                  'VueJS',
-                  'AngularJS',
-                ]}
-              />
-            }
-          />
+            <section className="container">
+                <div className="row row--large-gutter@xl-up u-pad-ends-small@lg-up u-pad-ends-2xsmall@ms-down">
+                    <TechnologiesCard
+                        title={'Expertise in'}
+                        subtitle={'backend development'}
+                        description={
+                            'We can provide\n' +
+                            'language-agnostic development,\n' +
+                            'SOLID development, test-driven development, RESTFul API, scalable\n' +
+                            'architecture, third-party\n' +
+                            'integrations. Our passion is creating solid yet scalable solutions such\n' +
+                            'as Cloud/Backend Architecture,\n' +
+                            'API design and DevOps.'
+                        }
+                        WeUse={
+                            <WeUseCard
+                                stacks={[
+                                    'PHP',
+                                    'JS',
+                                    'NodeJs',
+                                    'C#',
+                                    'Rust',
+                                    'Python',
+                                    'Django',
+                                    'Java',
+                                    '.NET',
+                                    'React',
+                                    'VueJS',
+                                    'AngularJS',
+                                ]}
+                            />
+                        }
+                    />
+                    <TechnologiesCard
+                        title={'Expertise in'}
+                        subtitle={'backend development'}
+                        description={
+                            'We can provide\n' +
+                            'language-agnostic development,\n' +
+                            'SOLID development, test-driven development, RESTFul API, scalable\n' +
+                            'architecture, third-party\n' +
+                            'integrations. Our passion is creating solid yet scalable solutions such\n' +
+                            'as Cloud/Backend Architecture,\n' +
+                            'API design and DevOps.'
+                        }
+                        WeUse={
+                            <WeUseCard
+                                stacks={[
+                                    'PHP',
+                                    'JS',
+                                    'NodeJs',
+                                    'C#',
+                                    'Rust',
+                                    'Python',
+                                    'Django',
+                                    'Java',
+                                    '.NET',
+                                    'React',
+                                    'VueJS',
+                                    'AngularJS',
+                                ]}
+                            />
+                        }
+                    />
+                    <TechnologiesCard
+                        title={'Expertise in'}
+                        subtitle={'backend development'}
+                        description={
+                            'We can provide\n' +
+                            'language-agnostic development,\n' +
+                            'SOLID development, test-driven development, RESTFul API, scalable\n' +
+                            'architecture, third-party\n' +
+                            'integrations. Our passion is creating solid yet scalable solutions such\n' +
+                            'as Cloud/Backend Architecture,\n' +
+                            'API design and DevOps.'
+                        }
+                        WeUse={
+                            <WeUseCard
+                                stacks={[
+                                    'PHP',
+                                    'JS',
+                                    'NodeJs',
+                                    'C#',
+                                    'Rust',
+                                    'Python',
+                                    'Django',
+                                    'Java',
+                                    '.NET',
+                                    'React',
+                                    'VueJS',
+                                    'AngularJS',
+                                ]}
+                            />
+                        }
+                    />
+                    <TechnologiesCard
+                        title={'Expertise in'}
+                        subtitle={'backend development'}
+                        description={
+                            'We can provide\n' +
+                            'language-agnostic development,\n' +
+                            'SOLID development, test-driven development, RESTFul API, scalable\n' +
+                            'architecture, third-party\n' +
+                            'integrations. Our passion is creating solid yet scalable solutions such\n' +
+                            'as Cloud/Backend Architecture,\n' +
+                            'API design and DevOps.'
+                        }
+                        WeUse={
+                            <WeUseCard
+                                stacks={[
+                                    'PHP',
+                                    'JS',
+                                    'NodeJs',
+                                    'C#',
+                                    'Rust',
+                                    'Python',
+                                    'Django',
+                                    'Java',
+                                    '.NET',
+                                    'React',
+                                    'VueJS',
+                                    'AngularJS',
+                                ]}
+                            />
+                        }
+                    />
+                </div>
+            </section>
+            <FeedBack/>
         </div>
-      </section>
-    </div>
-  )
+    )
 }
 
 export default Home
